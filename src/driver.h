f85bc32b3cd85276c20eba2bb043360b06bac989
mazx <dyxdy@live.com>
2018-07-23 14:20:11 +0800

    driver.h

---
+++ src/driver.h
@@ -0,0 +1,23 @@
+#ifndef THKVS_DRIVER_H
+#define THKVS_DRIVER_H
+
+#include <cstdio>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+class Driver {
+private:
+	map <Node> nodeMap;
+	unsigned hash(String str) {}
+public:
+	Driver() {}
+	~Driver() {}
+	void put() {}
+	void get() {}
+}
+
+#endif
