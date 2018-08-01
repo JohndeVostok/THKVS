87f0f254887c89f9c42a3f0df4d93c0c5dbf1aa6
Captain_Mushroom <wuhuajun132@gmail.com>
2018-08-01 17:18:46 +0800

    modified data.cpp and add cache

--- src/data.cpp
+++ src/data.cpp
@@ -1,5 +1,4 @@
 #include "data.h"
-#include "msg_handler.hpp"
 #include <string>
 #include <cstdio>
 #include <time.h>
@@ -13,9 +12,6 @@ long long getTime()
     gettimeofday(&now, NULL);
 	long long real = now.tv_sec * 1000000 + now.tv_usec;
     time_t timep;
-    time (&timep);
-    char tmp[64];
-    strftime(tmp, sizeof(tmp), "%Y-%m-%d %H:%M:%S",localtime(&timep) );
     return real;
 }
 
@@ -35,14 +31,47 @@ void Data::get(int id, string ip, int port, string key)
 void Data::get_return(int id, string ip, int port, int status, string value, long long time_stamp)
 {
 	msgHandler::sendGetRet(id, ip, port, status, value, time_stamp);
-	//printf("get_return id:%d status:%s value:%s time_stamp:%lld\n", id, status.c_str(), value.c_str(), time_stamp);
+	//printf("get_return id:%d status:%d value:%s time_stamp:%lld\n", id, status, value.c_str(), time_stamp);
 	//return "get_return success";
 }
 void Data::put_return(int id, string ip, int port, int status)
 {
 	msgHandler::sendPutRet(id, ip, port, status);
-	//printf("put_return id:%d value:%s\n", id, status.c_str());
+	//printf("put_return id:%d status:%d\n", id, status);
 	//return "put_return success";
+}
+
+string Data::conkey(int key)
+{
+	string ret = "";
+	while (key > 0)
+	{
+		ret = char(key % 10 + 48) + ret;
+		key = key / 10;
+	}
+	return ret;
+}
+
+string Data::conkey2(long long key)
+{
+	string ret = "";
+	while (key > 0)
+	{
+		ret = char(key % 10 + 48) + ret;
+		key = key / 10;
+	}
+	return ret;
+}
+
+long long tolonglong(string key)
+{
+	int length = key.size();
+	long long time_stamp = 0;
+	for (int i = 0; i <= length - 1; i++)
+	{
+		time_stamp = time_stamp * 10 + key[i] - '0';
+	}
+	return time_stamp;
 }
 
 void Data::run()
@@ -50,7 +79,14 @@ void Data::run()
 	string key = "0";
 	string value = "0";
 	while(true) {
+		//printf("running\n");
 		KeyValue kv = KeyValue(0, key, 0, key, value, 0);
+		/*
+		if (que.empty() == true)
+		{
+			continue;
+		}
+		*/
 		que.pop(kv);
 		//put 
 		Value V;
@@ -61,22 +97,43 @@ void Data::run()
 		{
 			//0 means success
 			int status = 1;
-			umap[kv.key] = V;
+			//umap[kv.key] = V;
+			//printf("kv.key:%s kv.value:%s\n", kv.key.c_str(), kv.value.c_str());
+			hs.put(kv.key, kv.value + conkey2(V.time_stamp));
+			//printf("whole_setence:%s\n", (kv.value + conkey2(V.time_stamp)).c_str());
 			status = 0;
 			put_return(kv.id, kv.ip, kv.port, status);
 		}
+
 		if (kv.op == 2)
 		{
 			int status = 1;
-			int time_stamp = 0;
-			if (umap.find(kv.key) == umap.end())
+
+			string ret = hs.get(kv.key);
+			//printf("!!!RET!!!!:%s\n", ret.c_str());
+			string value = "";
+			string time_stamp = "";
+
+			int length = ret.size();
+			int cnt = 0;
+			for (int i = length - 1; i >= 0; i--)
+			{
+				cnt += 1;
+				if (cnt >= 17)
+					value = ret[i] + value;
+				else
+					time_stamp = ret[i] + time_stamp;
+			}
+
+			//printf("value:%s\n", value.c_str());
+			//printf("time_stamp:%s\n", time_stamp.c_str());
+
+			if (value == "")
 				status = 1;
 			else
-			{
-				V = umap[kv.key];
 				status = 0;
-			}
-			get_return(kv.id, kv.ip, kv.port, status, V.value, V.time_stamp);
+
+			get_return(kv.id, kv.ip, kv.port, status, value, tolonglong(time_stamp));
 		}
 	}
 }
